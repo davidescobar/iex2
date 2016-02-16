4558de85dcdfe8548dbfa175f55decf3cacea8b8
J. David Escobar <david.escobar@lendstreet.com>
2016-02-15 22:57:10 -0800

    Automatically loading .crt and .key files from paths.yml. Now using cabal for building and running.

--- iex2.hs
+++ iex2.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Concurrent
 import Control.Exception (displayException, try)
 import Control.Monad
+import Data.Int
+import Data.List
 import Data.Maybe
+import Data.String.Utils
+import System.Console.ANSI
 import System.Console.Haskeline
 import System.Directory
 import System.Environment
@@ -11,6 +17,9 @@ import System.Exit
 import System.FilePath
 import System.IO
 import System.Process
+
+import qualified Data.Map as Map
+import qualified System.Posix as Posix
 
 
 main :: IO ()
@@ -28,54 +37,109 @@ main = do
     Right workingDirContents
       | "mix.exs" `elem` workingDirContents -> do
           -- Set environment variables
-          let envVars = if mixEnv /= "dev"
+          sslFilePaths <- getSSLPaths workingDir
+          let crtFilePath = fromMaybe "" (Map.lookup "crt" sslFilePaths)
+              keyFilePath = fromMaybe "" (Map.lookup "key" sslFilePaths)
+              envVars = if mixEnv /= "dev"
                           then [ ("MIX_ENV", mixEnv),
-                                 ("DEEP_BLUE_SSL_KEY_PATH", "/Users/davidescobar/Downloads/wildcard_lendstreet_com_pkg/lendstreet.com.key"),
-                                 ("DEEP_BLUE_SSL_CERT_PATH", "/Users/davidescobar/Downloads/wildcard_lendstreet_com_pkg/lendstreet.com.crt") ]
+                                 ("DEEP_BLUE_SSL_KEY_PATH", crtFilePath),
+                                 ("DEEP_BLUE_SSL_CERT_PATH", keyFilePath) ]
                           else [ ("MIX_ENV", mixEnv) ]
-              historyFilePath = workingDir </> "iex2.log"
+              historyFilePath = workingDir </> ".iex2.log"
           mapM_ (\(var, value) -> setEnv var value) envVars
 
           (hIn, hOut, hErr, pid) <- runInteractiveProcess "iex" [ "-S", "mix" ] (Just workingDir) Nothing
           hSetBinaryMode hIn False
           hSetBinaryMode hOut False
-          hSetBuffering hIn LineBuffering
+          hSetBuffering hIn NoBuffering
           hSetBuffering hOut NoBuffering
           hSetBuffering stdout NoBuffering
           runInteractiveConsole hIn hOut hErr pid historyFilePath
       | otherwise -> putStrLn $ "Could not find a mix.exs file in '" ++ workingDir ++ "'.\n"
 
 
+getSSLPaths :: String -> IO (Map.Map String String)
+getSSLPaths iexDir = do
+  iexDirExists <- doesDirectoryExist iexDir
+  if iexDirExists
+    then do
+      let pathsYAMLFile = iexDir </> "config/paths.yml"
+      yamlLines <- readFile pathsYAMLFile >>= return . lines
+      let crtFileMap = case find (isInfixOf "crt:") yamlLines of
+                         Just line -> Map.fromList [ ("crt", strip $ last $ split ":" line) ]
+                         Nothing -> Map.empty
+      let keyFileMap = case find (isInfixOf "key:") yamlLines of
+                          Just line -> Map.fromList [ ("key", strip $ last $ split ":" line) ]
+                          Nothing -> Map.empty
+      return $ Map.union crtFileMap keyFileMap
+    else return Map.empty
+
+
+reduceLogFileSize :: Int64 -> FilePath -> IO ()
+reduceLogFileSize maxSizeLimit historyFilePath = do
+  exists <- doesFileExist historyFilePath
+  if exists
+    then do
+      currentSize <- Posix.getFileStatus historyFilePath >>= return . fromIntegral . Posix.fileSize
+      if currentSize > maxSizeLimit
+        then do
+          logFileLines <- readFile historyFilePath >>= return . lines
+          let newNumLines = (length logFileLines) `div` 2
+              newHistoryFile = (takeDirectory historyFilePath) </> ".new_iex2.log"
+              newLogContent = unlines $ take newNumLines logFileLines
+          writeFile newHistoryFile newLogContent
+          removeFile historyFilePath
+          renameFile newHistoryFile historyFilePath
+        else return ()
+    else return ()
+
+
 runInteractiveConsole :: Handle -> Handle -> Handle -> ProcessHandle -> FilePath -> IO ()
 runInteractiveConsole hIn hOut hErr pid historyFilePath = do
-  _ <- forkIO $ runOutputLoop pid hOut hErr
-  _ <- forkIO $ runInputLoop pid hIn historyFilePath
+  reduceLogFileSize (2^20) historyFilePath
+  _ <- forkIO $ runOutputLoop hOut
+  _ <- forkIO $ runInputLoop hIn historyFilePath
+  _ <- forkIO $ runErrorLoop hErr
   forever $ do
     threadDelay 1000000 -- 1 second
     exitCodeMaybe <- getProcessExitCode pid
     when (isJust exitCodeMaybe) (exitWith $ fromJust exitCodeMaybe)
 
 
-runInputLoop :: ProcessHandle -> Handle -> FilePath -> IO ()
-runInputLoop pid hIn historyFilePath = do
+runInputLoop :: Handle -> FilePath -> IO ()
+runInputLoop hIn historyFilePath = do
   let settings = defaultSettings { historyFile = Just historyFilePath }
   maybeLine <- runInputT settings (getInputLine "")
   success <- try $ hPutStrLn hIn (fromMaybe "" maybeLine) :: IO (Either SomeException ())
   case success of
     Left err -> putStrLn $ displayException err
-    Right _ -> runInputLoop pid hIn historyFilePath
+    Right _ -> runInputLoop hIn historyFilePath
 
 
-runOutputLoop :: ProcessHandle -> Handle -> Handle -> IO ()
-runOutputLoop pid hOut hErr = do
-  contentsOrError <- try $ hGetContents hOut :: IO (Either SomeException String)
-  case contentsOrError of
-    Left contErr -> do
-      putStrLn $ displayException contErr
-      errorContentsOrError <- try $ hGetContents hErr :: IO (Either SomeException String)
-      case errorContentsOrError of
-        Left errContErr -> putStrLn $ displayException errContErr
-        Right errorContents -> putStrLn errorContents
-    Right contents -> do
-      putStrLn contents
-      runOutputLoop pid hOut hErr
+runOutputLoop :: Handle -> IO ()
+runOutputLoop hOut = do
+  hOutReady <- hReady hOut
+  if hOutReady
+    then do
+      setSGR [ Reset ]
+      contentsOrErr <- try $ hGetContents hOut :: IO (Either SomeException String)
+      case contentsOrErr of
+        Left contErr -> putStrLn $ displayException contErr
+        Right contents -> do
+          putStrLn contents
+          runOutputLoop hOut
+    else runOutputLoop hOut
+
+
+runErrorLoop :: Handle -> IO ()
+runErrorLoop hErr = do
+  hErrReady <- hReady hErr
+  if hErrReady
+    then do
+      setSGR [ SetColor Foreground Vivid Red ]
+      eitherErr <- try $ hGetContents hErr :: IO (Either SomeException String)
+      case eitherErr of
+        Left err -> putStrLn $ displayException err
+        Right errContents -> putStrLn errContents
+      setSGR [ Reset ]
+    else runErrorLoop hErr
